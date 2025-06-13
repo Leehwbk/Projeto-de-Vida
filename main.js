e12a4339222cf880b533a011199904e27fc252bf
Leehwbk <138231452+Leehwbk@users.noreply.github.com>
2025-06-13 08:51:09 -0300

    ok

--- main.js
+++ main.js
@@ -16,9 +16,12 @@ for (let i = 0; i < botoes.length; i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-03-01T00:00:00");
+const tempoObjetivo2 = new Date("2026-12-20T00:00:00");
+const tempoObjetivo3 = new Date("2027-01-01T00:00:00");
+const tempoObjetivo4 = new Date("2030-05-12T00:00:00");
 
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -27,10 +30,27 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-
+    
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+    if (tempoFinal > 0){
+        return dias + " dias " + horas +" horas " + minutos + " minutos " + segundos + " segundos ";
+    } else {
+        return "Prazo Finalizado";
+    }
 
-    return dias + " dias " + horas +" horas " + minutos + " minutos " + segundos + " segundos ";
 }
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
